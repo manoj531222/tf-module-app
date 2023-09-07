0c72a972a016bc639716f73c9ea6a82b1b7c7f04
manoj <gandhamanojkutty@gmail.com>
2023-09-07 14:06:35 +0530

    asg added

--- main.tf
+++ main.tf
@@ -4,19 +4,19 @@ resource "aws_security_group" "sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "app"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = var.allow_app_cidr
+    description = "APP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = var.allow_app_cidr
   }
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = var.bastion_cidr
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.bastion_cidr
   }
 
   egress {
@@ -32,24 +32,24 @@ resource "aws_security_group" "sg" {
   }
 }
 
+
 resource "aws_launch_template" "template" {
-  name_prefix   = "${var.name}-${var.env}-lt"
-  image_id      = data.aws_ami.ami.id
-  instance_type = var.instance_type
+  name_prefix            = "${var.name}-${var.env}-lt"
+  image_id               = data.aws_ami.ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
 }
 
 resource "aws_autoscaling_group" "asg" {
-  name               = "${var.name}-${var.env}-asg"
-  desired_capacity   = var.desired_capacity
-  max_size           = var.max_size
-  min_size           = var.min_size
+  name                = "${var.name}-${var.env}-asg"
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
   vpc_zone_identifier = var.subnet_ids
+
 
   launch_template {
     id      = aws_launch_template.template.id
     version = "$Latest"
   }
-
 }
-
