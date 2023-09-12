1dc365f2506e1aaadf5ce2ab74ac2dd199918c28
manoj <gandhamanojkutty@gmail.com>
2023-09-12 11:57:36 +0530

    code updated

--- main.tf
+++ main.tf
@@ -27,7 +27,9 @@ resource "aws_security_group" "sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags = merge(var.tags, { Name = "${var.name}-${var.env}-sg" })
+  tags = {
+    Name = "${var.name}-${var.env}-sg"
+  }
 }
 
 
@@ -51,7 +53,6 @@ resource "aws_autoscaling_group" "asg" {
     id      = aws_launch_template.template.id
     version = "$Latest"
   }
-
   dynamic "tag" {
     for_each = local.asg_tags
     content {
@@ -60,37 +61,11 @@ resource "aws_autoscaling_group" "asg" {
       value               = tag.value
     }
   }
-
 }
-
 resource "aws_lb_target_group" "main" {
   name     = "${var.name}-${var.env}-tg"
   port     = var.app_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   tags     = merge(var.tags, { Name = "${var.name}-${var.env}-tg" })
-}
-
-resource "aws_lb_listener_rule" "rule" {
-  listener_arn = var.listener_arn
-  priority     = var.listener_priority
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-
-  condition {
-    host_header {
-      values = [local.dns_name]
-    }
-  }
-}
-
-resource "aws_route53_record" "main" {
-  zone_id = var.domain_id
-  name    = local.dns_name
-  type    = "CNAME"
-  ttl     = 30
-  records = [var.lb_dns_name]
 }
